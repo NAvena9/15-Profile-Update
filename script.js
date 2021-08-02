7e4c37dde885dfbc8b4ca0a11b9adba5a7f965bf
NOat <nicolas.cedano.avena@gmail.com>
2021-08-01 21:53:49 -0500

    some changes

--- script.js
+++ script.js
@@ -50,7 +50,6 @@ $(document).ready(function () {
             "<img src='images/timed-quiz.png' class='card-img-top' alt='picture of timed quiz application'>" +
             "<div class='card-body text-center'>" +
             "<h5 class='card-title'>JS Timed-Quiz</h5>" +
-            // "<p class='card-text'>An application used to log your favorite burgers. Submit new burgers and devour them! Built with Node.js, Express, MySQL, Handlebars, and a custom ORM. </p>" +
             "<a target='_blank' href='https://github.com/NAvena9/04-APIs-Timed-Quiz' class='btn btn-info port-btn'>Repo</a>" +
             "<a target='_blank' href='https://navena9.github.io/04-APIs-Timed-Quiz/' class='btn btn-primary port-btn'>Demo</a>" +
             "</div>" +
@@ -61,7 +60,6 @@ $(document).ready(function () {
             "<img src='images/scheduler.png' class='card-img-top' alt='picture of the scheduler app'>" +
             "<div class='card-body text-center'>" +
             "<h5 class='card-title'>Scheduler App</h5>" +
-            // "<p class='card-text'>A platform that promotes collaboration by allowing users to share and explore open source applications.  Built with Node, Express, Handlebars, MySQL, and GitHub OAuth.</p>" +
             "<a target='_blank' href='https://github.com/NAvena9/05-Work-Day-Scheduler' class='btn btn-info port-btn'>Repo</a>" +
             "<a target='_blank' href='https://navena9.github.io/05-Work-Day-Scheduler/' class='btn btn-primary port-btn'>Demo</a>" +
             "</div>" +
@@ -72,7 +70,6 @@ $(document).ready(function () {
             "<img src='images/Project1.png' class='card-img-top center' alt='picture of pitch MyBookCloud'>" +
             "<div class='card-body text-center'>" +
             "<h5 class='card-title'>MyBookCloud</h5>" +
-            // "<p class='card-text'>An application that fetches third-party APIs and displays Books by Author and title. </p>" +
             "<a target='_blank' href='https://github.com/NAvena9/project1' class='btn btn-info port-btn'>Repo</a>" +
             "<a target='_blank' href='https://navena9.github.io/project1/' class='btn btn-primary port-btn'>Demo</a>" +
             "</div>" +
@@ -85,7 +82,6 @@ $(document).ready(function () {
             "<img src='images/weather.png' class='card-img-top' alt='picture of weather app'>" +
             "<div class='card-body text-center'>" +
             "<h5 class='card-title'>Weather App</h5>" +
-            // "<p class='card-text'>A timed code quiz application that allows users to test their JavaScript skills and save their scores to local storage.</p>" +
             "<a target='_blank' href='https://github.com/NAvena9/06-Weather-Dashboard' class='btn btn-info port-btn'>Repo</a>" +
             "<a target='_blank' href='https://navena9.github.io/06-Weather-Dashboard/' class='btn btn-primary port-btn'>Demo</a>" +
             "</div>" +
@@ -96,7 +92,6 @@ $(document).ready(function () {
             "<img src='images/pass_generator.png' class='card-img-top' alt='picture of Password Generator'>" +
             "<div class='card-body text-center'>" +
             "<h5 class='card-title'>Password Generator</h5>" +
-            // "<p class='card-text'>A weather dashboard application that enables the user to retrieve the current weather and 5-day forecast for cities across the United States.</p>" +
             "<a target='_blank' href='https://github.com/NAvena9/03-Password-Generator' class='btn btn-info port-btn'>Repo</a>" +
             "<a target='_blank' href='https://navena9.github.io/03-Password-Generator/' class='btn btn-primary port-btn'>Demo</a>" +
             "</div>" +
@@ -117,40 +112,8 @@ $(document).ready(function () {
 
         ).fadeIn(400);
 
-        /* const cards = $("div[id^='item']"); */
-        /* const cards = $(".card-style");
-
-        cards.on("mouseenter", function(){
-            cards.not($(this)).css("filter", "blur(5px)");
-        }).on("mouseleave", function(){
-            cards.css("filter", "blur(0px)");
-        }); */
-
     }
 
-    function populateDocumentation() {
-        $(".container").empty().hide().append(
-            "<div class='row'>" +
-            "<div class='col-md-6 mx-auto mt-5 text-center'>" +
-            "<p>One of the most important aspects of being a programmer is the ability to explain the technicalities of your code. Here are a few articles I've written and posted to dev.to.</p>" +
-            "</div>" +
-            "</div>" +
-            "<div class='row'>" +
-            "<div class='col-sm-6 mx-auto dev-row mt-4 mb-3 text-center'>" +
-            "<p>7 Oct 2020</p>" +
-            "<i class='fab fa-dev dev-icon mr-2'></i>" +
-            "<a href='https://dev.to/twkirkpatrick/how-i-felt-entering-bootcamp-vs-how-i-feel-after-2-months-220i' class='article' target='_blank'>How I felt entering bootcamp vs. how I feel after 2 months</a>" +
-            "</div>" +
-            "</div>" +
-            "<div class='row'>" +
-            "<div class='col-sm-6 mx-auto dev-row mt-2 text-center'>" +
-            "<p>14 Nov 2020</p>" +
-            "<i class='fab fa-dev dev-icon mr-2'></i>" +
-            "<a href='https://dev.to/twkirkpatrick/var-let-and-const-what-s-the-difference-2524' class='article' target='_blank'>Var, Let, and Const...What's The Difference?</a>" +
-            "</div>" +
-            "</div>"
-        ).fadeIn(400);
-    }
 
     $(".nav-work").on("click", function () {
         $(".about-page").hide();
@@ -167,7 +130,6 @@ $(document).ready(function () {
         $(".about-page").hide();
         $(".header").hide();
         $(".portfolio-page").hide();
-
         populateMain();
     })
 
@@ -175,8 +137,7 @@ $(document).ready(function () {
         $(".about-page").hide();
         $(".header").hide();
         $(".portfolio-page").hide();
-
-        populateDocumentation();
+        // populateDocumentation();
     })
 
 
@@ -191,10 +152,10 @@ $(document).ready(function () {
         populatePortfolio();
     })
 
-    $(document).on("click", ".docs", function () {
-        $(".navbar").removeClass("hide").hide().fadeIn(1000);
-        populateDocumentation();
-    })
+    // $(document).on("click", ".docs", function () {
+    //     $(".navbar").removeClass("hide").hide().fadeIn(1000);
+    //     populateDocumentation();
+    // })
 
 
 
